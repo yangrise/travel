c7bbc6e461edd1461b7a39d4354253b22a9877b0
yangrise <iyangrise@gmail.com>
2018-07-02 21:26:33 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img :src="item.imgUrl" alt="" class="swiper-img">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3b/ef86879aa50e3002.jpg_750x200_2a108508.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/60/2eee57f623edfa02.jpg_750x200_74566baa.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #fff !important
+    .wrapper
+        width 100%
+        height 0
+        overflow hidden
+        padding-bottom 31.25%
+        .swiper-img
+            width 100%
+</style>
