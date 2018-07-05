566c695956225d868fd30c6ac67d1e9b721caf58
yangrise <iyangrise@gmail.com>
2018-07-05 21:38:45 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for='item of recommendList' :key='item.id'>
+        <img :src="item.imgUrl" alt="" class="item-img">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1609/7a/7ae8ee7831836095a3.water.jpg_200x200_36727f69.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1609/7a/7ae8ee7831836095a3.water.jpg_200x200_36727f69.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1609/7a/7ae8ee7831836095a3.water.jpg_200x200_36727f69.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，浪漫的海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item
+    display flex
+    height 1.9rem
+    overflow hidden
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding 0.1rem
+      min-width 0
+      .item-title
+        line-height 0.54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
+      .item-button
+        background #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        margin-top 0.16rem
+        color #fff
+        line-height .44rem
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for='item of recommendList' :key='item.id'>
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" alt="" class="item-img">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1606/85/c6b6bad1b977e.jpg_r_640x214_5c638941.jpg',
+          title: '漂流勇进，烧烤露营',
+          desc: '轻松水上漂，任我乐逍遥'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1606/85/c6b6bad1b977e.jpg_r_640x214_5c638941.jpg',
+          title: '漂流勇进，烧烤露营',
+          desc: '轻松水上漂，任我乐逍遥'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1606/85/c6b6bad1b977e.jpg_r_640x214_5c638941.jpg',
+          title: '漂流勇进，烧烤露营',
+          desc: '轻松水上漂，任我乐逍遥'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    height 0
+    overflow hidden
+    padding-bottom 33.9%
+    .item-img
+      width 100%
+  .item-info
+    padding 0.1rem
+    .item-title
+      line-height 0.54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+</style>
