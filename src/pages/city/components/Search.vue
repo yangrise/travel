1096c715f40165a341a5a2ee4379ca555f0be7fa
yangrise <iyangrise@gmail.com>
2018-07-08 22:06:28 +0800

    add search to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  </div>
+</template>
+<script>
+export default {
+  name: 'citySearch'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height 0.72rem
+    background $bgColor
+    padding 0 .1rem
+    .search-input
+      height 0.62re
+      line-height 0.62rem
+      width 100%
+      text-align center
+      border-radius .06rem
+      color #666
+      padding 0 .1rem
+      box-sizing border-box
+</style>
